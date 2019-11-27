4f9122486c6a6bdf6dfd7ff09b58485fcba167f0
tyrspawn <33181041+tyrspawn@users.noreply.github.com>
2019-11-26 19:21:31 -0800

    Release refactor (#9)
    * WIP git repo checkout approach
    
    Having an issue where the output zip includes the subfolder "repoCheckOutTemp" and includes all the git files. Probably logical issue in zipdir function
    
    * Refactor
    
    * Tweak output format

--- make_release.py
+++ make_release.py
@@ -1,38 +1,68 @@
+# Run from cloned local repo, has a dependency on being run from an LFS enabled repo directory
 import os
 import zipfile
+import git
+import shutil
+import time
+import stat
+from git import Repo
+
+tempWorkingDir = "../repoCheckOutTemp/Add-On Kuwait UOAF"
+tempWorkingDirRoot = "../repoCheckOutTemp/"
+'''
+Error handler function
+It will try to change file permission and call the calling function again,
+'''
+def handleError(func, path, exc_info):
+    print('Handling Error for file ' , path)
+    print(exc_info)
+    # Check if file access issue
+    if not os.access(path, os.W_OK):
+       # Try to change the permision of file
+       os.chmod(path, stat.S_IWUSR)
+       # call the calling function again
+       func(path)
+
+def deleteFolderAndFiles(path):
+    try:
+        print("Trying to delete ",path)
+        shutil.rmtree(path, ignore_errors=False, onerror=handleError)
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))    
+
+deleteFolderAndFiles(tempWorkingDirRoot)
+
+print ("git clone starting...")
+time.sleep(2) # Had to do this because git clone is throwing access denied without it, probably due to a race condition with rmtree
+Repo.clone_from("https://github.com/tyrspawn/kuwaitmodded", tempWorkingDir)
+print("git clone done")
+
+#copy readme.md to working dir root
+shutil.copyfile(os.path.join(tempWorkingDir, "README.md"), os.path.join(tempWorkingDirRoot, "README.md"))
 
 def get_theater_version():
-    with open(os.path.join('TerrData', 'theaterdefinition', 'kuwait.tdf'), 'r') as tdf:
+    with open(os.path.join(tempWorkingDir, 'TerrData', 'theaterdefinition', 'kuwait.tdf'), 'r') as tdf:
         for line in tdf:
             if line.startswith('desc'):
                 version_token = line.split()[-1].strip()
+                print ("Preparing version ", version_token)
                 return version_token
     raise ValueError("Version not found in theater")
 
+ver = get_theater_version()
 
+# delete black list files from working dir
+blackListFiles = [".gitattributes",".gitignore", "make_release.py"]
 
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, _, files in os.walk(path):
-        for file in files:
-            if not file.startswith('.git') and not file.endswith('.py'):
-                filesystem_path = os.path.normpath(os.path.join(root, file))
-                if filesystem_path.startswith('.git'):
-                    continue
-                if filesystem_path.lower().startswith('campaign'):
-                    if filesystem_path.lower().endswith('.cam'):
-                        f = file.lower()
-                        if f not in ('save0.cam', 'save1.cam', 'save2.cam'):
-                            continue
+for file in blackListFiles:
+    if os.path.isfile(os.path.join(tempWorkingDir,file)):
+        os.remove(os.path.join(tempWorkingDir,file))
 
-                    
-                archive_path = os.path.join('Add-On Kuwait UOAF', filesystem_path)
-                print(filesystem_path)
-                ziph.write(filesystem_path, arcname=archive_path)
-    ziph.write('README.md')
+# delete .git dir from working dir
+deleteFolderAndFiles(os.path.join(tempWorkingDir,".git"))
 
-if __name__ == '__main__':
-    ver = get_theater_version()
-    with zipfile.ZipFile(os.path.join('..', 'Kuwait UOAF.{}.zip'.format(ver)), 'w', zipfile.ZIP_DEFLATED) as zipf:
-        zipdir('.', zipf)
-    
+print ("Starting zip archive to ", os.path.dirname(os.path.abspath(__file__ + "/..")))
+shutil.make_archive((os.path.join('..', 'Kuwait UOAF.{}'.format(ver))), 'zip', tempWorkingDirRoot)
+print ("Finished archive")
+
+deleteFolderAndFiles(tempWorkingDirRoot)
