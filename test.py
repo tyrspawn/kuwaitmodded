42b5b0a784ffdaea17da80a9fa9dc14468fd97a7
Chris Krause <krause@krauselabs.net>
2019-12-04 21:58:00 -0800

    Update test.py

--- test.py
+++ test.py
@@ -12,29 +12,6 @@ while True:
     except:
         break
 
-gauth = GoogleAuth()
-# Try to load saved client credentials
-gauth.LoadCredentialsFile("mycreds.txt")
-if gauth.credentials is None:
-    # Authenticate if they're not there
-    gauth.LocalWebserverAuth()
-elif gauth.access_token_expired:
-    # Refresh them if expired
-    gauth.Refresh()
-else:
-    # Initialize the saved creds
-    gauth.Authorize()
-# Save the current credentials to a file
-gauth.SaveCredentialsFile("mycreds.txt")
-
-drive = GoogleDrive(gauth)
-
-file_path = "readme.pdf"
-file1 = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": "1LT_8TnwcvA38N4SjMD8OQmJ3RgYjyFuz"}]})
-file1.SetContentFile(file_path)
-file1.Upload()
-
-
 if (makeRelease == 1):
 
     from pydrive.auth import GoogleAuth
